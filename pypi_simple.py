3a448eb75bafb640c732e982065ef431f801220b
John T. Wodder II <git@varonathe.org>
2018-08-28 16:41:42 +0000

    Packaging

--- pypi_simple.py
+++ pypi_simple.py
@@ -1,3 +1,15 @@
+"""
+PyPI Simple Repository API client library
+
+Visit <https://github.com/jwodder/pypi-simple> for more information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'pypi-simple@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/pypi-simple'
+
 import attr
 
 PYPI_SIMPLE_ENDPOINT = 'https://pypi.org/simple/'
---
+++ setup.py
@@ -0,0 +1,14 @@
+from   os.path    import dirname, join
+import re
+from   setuptools import setup
+
+with open(join(dirname(__file__), 'pypi_simple.py')) as fp:
+    for line in fp:
+        m = re.search(r'^\s*__version__\s*=\s*([\'"])([^\'"]+)\1\s*$', line)
+        if m:
+            version = m.group(2)
+            break
+    else:
+        raise RuntimeError('Unable to find own __version__ string')
+
+setup(version=version)
