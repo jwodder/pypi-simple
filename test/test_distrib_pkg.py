7a4fa9b1887807b472a749ebafea786fa85b97b5
John T. Wodder II <git@varonathe.org>
2019-05-09 20:01:54 +0000

    This test has been pointless since commit f1f226b

--- test/test_distrib_pkg.py
+++ test/test_distrib_pkg.py
@@ -1,20 +1,6 @@
 import pytest
 from   pypi_simple import DistributionPackage
 
-def test_filename_parsed():
-    pkg = DistributionPackage(
-        filename='qypi-0.1.0-py3-none-any.whl',
-        project='qypi',
-        version='0.1.0',
-        package_type='wheel',
-        url="https://files.pythonhosted.org/packages/82/fc/9e25534641d7f63be93079bc07fa92bab136ddf5d4181059a1308a346f96/qypi-0.1.0-py3-none-any.whl#sha256=da69d28dcd527c0e372b3fa7b92fc333b327f8470175f035abc4e351b539189f",
-        requires_python=None,
-        has_sig=False,
-    )
-    assert pkg.project == 'qypi'
-    assert pkg.version == '0.1.0'
-    assert pkg.package_type == 'wheel'
-
 @pytest.mark.parametrize('fragment', ['', '#', '#sha256', '#sha256='])
 def test_get_no_digests(fragment):
     pkg = DistributionPackage(
